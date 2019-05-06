d5ae91c3f1cdcd19b0d6cb6ed5989d1b7352e0ff
DESKTOP-5TBM399\lx <346880761@qq.com>
2019-05-06 18:04:41 +0800

    no message

---
+++ vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <child1></child1>
+  </div>
+</template>
+
+<script>
+  import child1 from './components/child1'
+export default {
+  name: 'App',
+  components:{
+    child1
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/child1.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>头部信息</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "child1"
+    }
+</script>
+
+<style scoped>
+
+</style>
